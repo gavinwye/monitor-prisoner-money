e9e3846148f0d57859ab5bcdce6a17fe8118c6d0
janestead <45178435+janestead@users.noreply.github.com>
2018-11-20 15:16:08 +0000

    Update routes-content.js

--- app/routes-content.js
+++ app/routes-content.js
@@ -4,7 +4,7 @@
 		// add key/pairs like the line below
 		// variable: 'content',
 		notificationsHeadingOne: 'Manage notifications',
-		notificationsLedeParagraph: '',
+		notificationsLedeParagraph: 'All intelligence is available on the tool but you can be notified about specific transactions you want to monitor.',
 		notificationsBulletHeading: 'All intelligence is available on the tool but you can be notified about specific transactions you want to monitor. You can be notified about:',
 		notificationsBulletListItem1: 'credits and disbursements',
 		notificationsBulletListItem2: 'frequency of transactions',
