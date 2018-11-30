a01b18948dafba1adf9acbb0fa5cfd1a49dde4ad
Gavin Wye <gavin@gavinwye.com>
2018-11-30 16:00:28 +0100

    Added the add another javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,19 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+// This adds another of the compoent defined after the id of the same thing
+  $("#addAnother").click(function(){
+    $("#prisonInput").append(
+      '<div id="prisonInput">' +
+      '<div class="govuk-form-group">' +
+      '<label class="govuk-label" for="name">' +
+      'Prison name' +
+      '</label>' +
+      '<input class="govuk-input" id="name" name="name" type="text">' +
+      '</div>' +
+      '</div>'
+    );
+  });
+
 })
