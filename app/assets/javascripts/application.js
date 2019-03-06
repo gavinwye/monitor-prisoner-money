02b617ede9f9babae440427cc3e9c88b707a4c93
Gavin Wye <gavin@gavinwye.com>
2019-03-06 16:23:03 +0000

    added remove link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,7 +32,8 @@ $(document).ready(function () {
       '<label class="govuk-label" for="name">' +
         'Prison name' +
       '</label>' +
-      '<input class="govuk-input govuk-!-margin-bottom-5" id="event-name" name="event-name" type="text">' +
+      '<input class="govuk-input govuk-input--width-20 govuk-!-margin-bottom-5" id="event-name" name="event-name" type="text">' +
+      '<a href="" class="remove-list-item remove-link govuk-link govuk-link--no-visited-state">Remove</a>' +
       '</div>'
           );
   });
