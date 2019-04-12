7235f0acb385216f00d8f7317d75471510c5149c
Gavin Wye <gavin@gavinwye.com>
2019-04-12 14:27:14 +0200

    prisoners search

--- app/views/prisoners/routes.js
+++ app/views/prisoners/routes.js
@@ -1,18 +1,24 @@
 module.exports = function (router, content) {
 
-  router.get('/credits/routes', function(req, res) {
-
-      res.render('/credits/',
-          {
-              courts: utils.getcreditsList()
-          });
-  });
+  router.post('/prisoners/', function (req, res) {
+    res.redirect('/prisoners/results')
+  })
 
   // This is for the credits data
   router.get('/prisoners/', function(req, res) {
     // location of credit data json file
     var prisonerData = require('../../data/prisoner_list.json');
     res.render('prisoners/index',
+    {
+      prisoners: prisonerData
+    });
+  });
+
+  // This is for the credits data
+  router.get('/prisoners/results', function(req, res) {
+    // location of credit data json file
+    var prisonerData = require('../../data/prisoner_list.json');
+    res.render('prisoners/results',
     {
       prisoners: prisonerData
     });
