65a739bd0c52a495032a9828afd89b76b7803677
Gavin Wye <gavin@gavinwye.com>
2019-04-12 14:12:42 +0200

    Disbursments search

--- app/views/disbursements/routes.js
+++ app/views/disbursements/routes.js
@@ -1,18 +1,24 @@
 module.exports = function (router, content) {
 
-  router.get('/credits/routes', function(req, res) {
-
-      res.render('/credits/',
-          {
-              courts: utils.getcreditsList()
-          });
-  });
+  router.post('/disbursements/', function (req, res) {
+    res.redirect('/disbursements/results')
+  })
 
   // This is for the credits data
   router.get('/disbursements/', function(req, res) {
     // location of credit data json file
     var disbursementData = require('../../data/disbursement_list.json');
     res.render('disbursements/index',
+    {
+      disbursements: disbursementData
+    });
+  });
+
+  // This is for the credits data
+  router.get('/disbursements/results', function(req, res) {
+    // location of credit data json file
+    var disbursementData = require('../../data/disbursement_list.json');
+    res.render('disbursements/results',
     {
       disbursements: disbursementData
     });
