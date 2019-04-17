a9974b3aa3218ad6c552d5f9ef54af91ecf1ea25
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-04-17 16:48:46 +0100

    Improve object searches

--- app/views/prisoners/routes.js
+++ app/views/prisoners/routes.js
@@ -18,9 +18,20 @@ module.exports = function (router, content) {
   router.get('/prisoners/results', function(req, res) {
     // location of credit data json file
     var prisonerData = require('../../data/prisoner_list.json');
+    let terms = (req.session.data.creditSearch || '').trim().split(/\s+/).map(term => term.toLowerCase())
+    let prisoners = prisonerData.filter(function (prisoner) {
+      for (let term of terms) {
+        for (let field of [prisoner.prisoner_name, prisoner.prisoner_number]) {
+          if ((field || '').toLowerCase().indexOf(term) >= 0) {
+            return true
+          }
+        }
+      }
+      return false
+    })
     res.render('prisoners/results',
     {
-      prisoners: prisonerData
+      prisoners: prisoners
     });
   });
 
