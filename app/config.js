dce39b788a000ac432cbe32647fb61152336fb22
Gavin Wye <gavin@gavinwye.com>
2019-05-01 11:32:07 +0100

    Changed name in header

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Prisoner money intelligence',
+  serviceName: 'Monitor prisoner money',
 
   // Default port that prototype runs on
   port: '3000',
