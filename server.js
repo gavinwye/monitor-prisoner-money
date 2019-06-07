19e4eb9603d00d8a59b7339f12e19ec77c865971
Gavin Wye <gavin@gavinwye.com>
2019-06-07 12:01:30 +0200

    Added the MOJ frontend

--- server.js
+++ server.js
@@ -113,6 +113,8 @@ app.use('/public', express.static(path.join(__dirname, '/public')))
 
 // Serve govuk-frontend in from node_modules (so not to break pre-extenstions prototype kits)
 app.use('/node_modules/govuk-frontend', express.static(path.join(__dirname, '/node_modules/govuk-frontend')))
+app.use('/assets', express.static(path.join(__dirname, '/node_modules/@ministryofjustice/frontend/assets')))
+
 
 // Set up documentation app
 if (useDocumentation) {
