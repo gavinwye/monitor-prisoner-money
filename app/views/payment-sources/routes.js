c8673cc6861911979cc4bb468403fb36ccecc688
Gavin Wye <gavin@gavinwye.com>
2019-06-24 17:17:07 +0200

    Added results in other prisons

--- app/views/payment-sources/routes.js
+++ app/views/payment-sources/routes.js
@@ -5,7 +5,7 @@ module.exports = function (router, content) {
   })
 
   router.post('/payment-sources/results', function (req, res) {
-    res.redirect('/payment-sources/')
+    res.redirect('/payment-sources/global-results')
   })
 
   // This is for the credits data
@@ -34,6 +34,27 @@ module.exports = function (router, content) {
       return false
     })
     res.render('payment-sources/results',
+    {
+      payment_sources: paymentSources
+    });
+  });
+
+  // This is for the credits data
+  router.get('/payment-sources/global-results', function(req, res) {
+    // location of credit data json file
+    var paymentSourceData = require('../../data/payment_source_list.json');
+    let terms = (req.session.data.paymentSourceSearch || '').trim().split(/\s+/).map(term => term.toLowerCase())
+    let paymentSources = paymentSourceData.filter(function (paymentSource) {
+      for (let term of terms) {
+        for (let field of [paymentSource.payment_source_name, paymentSource.payment_source_email]) {
+          if ((field || '').toLowerCase().indexOf(term) >= 0) {
+            return true
+          }
+        }
+      }
+      return false
+    })
+    res.render('payment-sources/global-results',
     {
       payment_sources: paymentSources
     });
