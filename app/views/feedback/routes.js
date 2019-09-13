1d4fe03881765c0656ec31f8f034f6150ca197af
Gavin Wye <gavin@gavinwye.com>
2019-09-13 16:48:31 +0200

    Updated content on the success page

--- app/views/feedback/routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (router, content) {
-
-
-  router.post('/feedback/index', function (req, res) {
-    res.redirect('/feedback/success')
-  })
-
-  // END__######################################################################################################
-}
